2a81440115f50d2311d90a794d43d54a57fe3e57
laetitia123 <uwizelaetitia@gmail.com>
2019-10-21 17:46:21 +0200

    profile test added.

--- photos/test.py
+++ photos/test.py
@@ -30,3 +30,21 @@ class ImageTestClass(TestCase):
         images = Image.objects.all()
         self.assertEqual(len(images),1)
    
+class Profiletest(TestCase):
+    # numusers = 10
+
+    # def setUp(self):
+    #     self.tempprofiles = []
+    #     for i in range(self.numusers):
+    #         tmp = Profile()
+    #         tmp.save()
+    #         self.tempprofiles.append(tmp)
+
+    # def test_follow(self):
+    #     follower = Profile()
+    #     follower.save()
+    #     for i in self.tempprofiles:
+    #         follower.follow(i)
+    #         self.assertEquals(follower.following.count(), self.numusers)
+    #     for u in self.tempprofiles:
+    #         self.assertEquals(u.followers.count(), 1)
