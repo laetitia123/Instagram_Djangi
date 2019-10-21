3d205e7e6d5cb9950753b255bca7c4381a1bcefa
laetitia123 <uwizelaetitia@gmail.com>
2019-10-21 17:58:13 +0200

    cleaning code

--- photos/forms.py
+++ photos/forms.py
@@ -13,19 +13,11 @@ class ProfileForm(forms.Form):
         model = Profile
         fields = ('Name', 'profile_picture', 'bio')
 
-
-
-
-
 class CommentForm(forms.ModelForm):
   
     class Meta:
         model = Comment
         fields = ('comment',)
-
-class NewsLetterForm(forms.Form):
-    your_name = forms.CharField(label='First Name',max_length=30)
-    email = forms.EmailField(label='Email')
 
 
 
