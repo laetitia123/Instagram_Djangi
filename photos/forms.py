046fc530a94e63d41d07b59f637c158e21e79dff
laetitia123 <uwizelaetitia@gmail.com>
2019-10-21 17:57:01 +0200

    cleaning code

--- photos/forms.py
+++ photos/forms.py
@@ -28,16 +28,7 @@ class NewsLetterForm(forms.Form):
     email = forms.EmailField(label='Email')
 
 
-class NewsLetterForm(forms.Form):
-    your_name = forms.CharField(label='First Name',max_length=30)
-    email = forms.EmailField(label='Email')
-class NewArticleForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        exclude = ['editor', 'pub_date']
-        widgets = {
-            'tags': forms.CheckboxSelectMultiple(),
-        }
+
 class uploadimageForm(forms.ModelForm):
     class Meta:
         model = Image
