61e6f24eb076b15a9b00718cd377357be89b7467
laetitia123 <uwizelaetitia@gmail.com>
2019-10-21 14:39:22 +0200

    style the home page

--- photos/forms.py
+++ photos/forms.py
@@ -15,20 +15,20 @@ class ProfileForm(forms.Form):
 
 
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        exclude = ('user', )
+# class PostForm(forms.ModelForm):
+#     class Meta:
+#         model = Image
+#         exclude = ('user', )
 
 
-class ProfileForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['Name'].widget = forms.TextInput()
+# class ProfileForm(forms.ModelForm):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['Name'].widget = forms.TextInput()
 
-    class Meta:
-        model = Profile
-        fields = ('Name', 'profile_picture', 'bio')
+#     class Meta:
+#         model = Profile
+#         fields = ('Name', 'profile_picture', 'bio')
 
 
 class CommentForm(forms.ModelForm):
--- photos/models.py
+++ photos/models.py
@@ -36,10 +36,10 @@ class Image(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='profile')
-    Name = models.TextField(default="Anonymous")
+    Name = models.TextField(default="Any")
     profile_picture = models.ImageField(
         upload_to='users/', default='users/user.png')
-    bio = models.TextField(default="Welcome Me!")
+    bio = models.TextField(default="Welcome !")
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
